97b9313f0110e83a2b32d61c65fe5af26efb337c
Reginald <reginaldgyasi62@gmail.com>
2026-01-03 20:04:34 +0000

    Day 1 ~ labs

---
+++ firstJsCodeLab.js
@@ -0,0 +1,20 @@
+// firstJsCodeLab.js
+// This is a simple JavaScript program that introduces a bot and shares some fun facts about programming.
+
+console.log("Hello! I'm your coding fun fact guide!")
+let botName = "Gen116";
+let botLocation = "your device";
+let favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage +".")
+
+let codingFact = "A fun fact about " + favoriteLanguage + " is that it can run in the browser and also on servers with Node.js.";
+console.log(codingFact)
+
+codingFact = "Another fun fact about " + favoriteLanguage + " is, ...I really do not know.[haha]"
+console.log(codingFact)
+
+codingFact = "A last fun fact about " + favoriteLanguage + " is, ...I am lost again. [sorry]"
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
