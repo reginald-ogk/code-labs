522a8d7b47ee8ba040bbf6716da7cc370207fabf
Reginald <reginaldgyasi62@gmail.com>
2026-01-09 20:40:16 +0000

    add golf score translator function

---
+++ scoreTranslater.js
@@ -0,0 +1,22 @@
+// Golf Code Translator
+// Returns the name associated with the number of strokes taken relative to par
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes === par - 1) {
+    return "Birdie";
+  } else if (strokes === par) {
+    return "Par";
+  } else if (strokes === par + 1) {
+    return "Bogey";
+  } else if (strokes === par + 2) {
+    return "Double Bogey";
+  } else {
+    return "Go Home!";
+  }
+}
